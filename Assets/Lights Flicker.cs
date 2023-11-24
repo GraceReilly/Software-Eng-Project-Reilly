eea4adda3e78c1a7df9b38d52d9da48ac1185e9e
iviichae7 <iviichae7@gmail.com>
2023-11-24 17:09:49 +0000

    Tried out a bunch of tutorials none worked in the end
    I followed a tutorial on a whiteboard for drawing and different tutorials for making a climable ladder. I did end up ending in some audio for a a flckering light, trying out reverb audio zone and also added in a interable flashlight. The tutorials followed for the above where the following - https://www.youtube.com/watch?v=sHE5ubsP-E8&t=464s&ab_channel=JustinPBarnett https://www.youtube.com/watch?v=1JZ62Us3RI0&ab_channel=FistFullofShrimp

---
+++ Assets/Lights Flicker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class LightsFlicker : MonoBehaviour
+{
+    public Light lightOB;
+    public AudioSource lightSound;
+    public float minTime;
+    public float maxTime;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LightsFlickering();
+    }
+    void LightsFlickering()
+    {
+        if (timer > 0)
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        
+        {
+            lightOB.enabled = !lightOB.enabled;
+            timer = Random.Range(minTime, maxTime);
+            lightSound.Play();
+        }
+    }
+}
